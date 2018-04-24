ddd747919c573a9f6f7ed786cef31f620b26f6bb
Bram Swinkels <33626711+Bsrawm@users.noreply.github.com>
2018-04-24 12:13:21 +0200

    Update home.php

--- Website/home.php
+++ Website/home.php
@@ -2,10 +2,36 @@
 $pageTitle = "Home";
 include_once "assets/head.php";
 ?>
-<body>
-hgruihgriughiurgh
-</body>
 
-<?php
+  <div class="row">
+    <div class="col-3"></div>
+    <div class="col-6">
+    <div class="container">
+      <img class="home-taxi" src="../image/Taxi.png" alt="Taxi">
+      <div class="image-text">
+        Ixat taxi<br/>
+        Het is beter dan lopen!</div>
+</div>
+      <input type="submit" value="Neem contact op">
+    </div>
+  </div>
+  <div class="row bg-white">
+
+    <div class="col-2"></div>
+    <div class="col-7">
+      <br/>
+      <h1>Ons concept</h1>
+
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+        eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, f ringilla vel, aliquet nec, vulputate
+        eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+        Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
+        porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+      </p>
+    </div>
+  </div>
+
+  <?php
 include_once "assets/footer.php";
 ?>
