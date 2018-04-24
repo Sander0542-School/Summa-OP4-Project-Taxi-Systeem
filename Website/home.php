e62290976c368ac60a48468c38e5c06fbb804963
Bram Swinkels <33626711+Bsrawm@users.noreply.github.com>
2018-04-24 13:11:56 +0200

    Update klanten pagina

--- Website/home.php
+++ Website/home.php
@@ -2,37 +2,38 @@
 $pageTitle = "Home";
 include_once "assets/head.php";
 ?>
-
-  <div class="row">
-    <div class="col-3"></div>
-    <div class="col-6">
-      <div class="home-container">
-        <img class="home-taxi" src="../image/Taxi.png" alt="Taxi">
-        <div class="image-text">
-          <b>Ixat taxi
-            <br/> Het is beter dan lopen!</b>
+  <div class="container">
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-6">
+        <div class="home-container">
+          <img src="../image/Taxi.png" alt="Taxi">
+          <div class="image-text">
+            <b>Ixat taxi
+              <br/> Het is beter dan lopen!</b>
+          </div>
+        <a href="/contact" class="btn btn-dark">Neem contact op</a>
         </div>
-      <a href="/contact" class="btn btn-dark">Neem contact op</a>
+      </div>
+    </div>
+    <div class="row bg-white">
+
+      <div class="col-2"></div>
+      <div class="col-8">
+        <br/>
+        <h1>Ons concept</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+          natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+          eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, f ringilla vel, aliquet nec, vulputate
+          eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+          Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
+          porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+        </p>
       </div>
     </div>
   </div>
-  <div class="row bg-white">
 
-    <div class="col-2"></div>
-    <div class="col-7">
-      <br/>
-      <h1>Ons concept</h1>
-
-      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-        eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, f ringilla vel, aliquet nec, vulputate
-        eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
-        Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-        porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-      </p>
-    </div>
-  </div>
-
-  <?php
+<?php
 include_once "assets/footer.php";
 ?>
--- Website/klanten.php
+++ Website/klanten.php
@@ -3,6 +3,39 @@ $pageTitle = "Klanten";
 include_once "assets/head.php";
 ?>
 
+
+<form method="POST">
+    <div class="container">
+    <div class="row">
+      <div class="col"></div>
+      <div class="col-9">
+        <h1>Registreer je nu als klant!</h1><br/>
+        <div class="row chauffeur">
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Naam" required><br/>
+            <input type="tel" class="form-control" placeholder="Mobiel Nummer" required><br/>
+            <input type="email" class="form-control" placeholder="E-mailadres" required><br/>
+          </div>
+          <div class="col-40px"></div>
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Gebruikersnaam" required><br/>
+            <input type="password" class="form-control" placeholder="Wachtwoord" required><br/>
+            <input type="password" class="form-control" placeholder="Herhaal Wachtwoord" required><br/>
+            <input type="submit" value="Registreer!" class="btn btn-block btn-dark">
+          </div>
+          <div class="col-40px"></div>
+          <div class="col">
+            <img src="../image/kech.png" alt="kech">
+          </div>
+        </div>
+      </div>
+      <div class="col"></div>
+    </div>
+  </div>
+  </form>
+          
+
+
 <?php
 include_once "assets/footer.php";
 ?>
