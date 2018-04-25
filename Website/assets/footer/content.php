e21bd76625bdc5bfbb41d4cd7ef6ff81912e4403
Sander Jochems <sanderjochems@hotmail.nl>
2018-04-25 12:13:49 +0200

    Update Footer Position Automatic

--- Website/assets/footer/content.php
+++ Website/assets/footer/content.php
@@ -1,3 +1,13 @@
-  	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDhUaFv5qwATzUG_DlgxbNCH1wXBa-B-PQ&callback=myMap"></script>
+    <script>
+      document.addEventListener("DOMContentLoaded", function(event) {
+        var footer = document.getElementById("footer");
+        if (document.body.clientHeight > window.innerHeight) {
+          footer.style.position = 'relative';
+        } else {
+          footer.style.position = 'fixed';
+        }
+      });
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDhUaFv5qwATzUG_DlgxbNCH1wXBa-B-PQ&callback=myMap"></script>
   </body>
 </html>
--- Website/klant-profiel.php
+++ Website/klant-profiel.php
@@ -3,44 +3,50 @@ $pageTitle = "Klant Profiel";
 include_once "assets/head.php";
 ?>
 
-<div class="container">
+	<div class="container">
 		<div class="row">
 			<div class="col-40px"></div>
-			<div class="col">
+			<div class="col-5">
 				<div class="row">
 					<div class="col">
 						<select class="custom-select" onchange="openProfiel(this)">
-							<option selected>Maak een keuze</option>
-							<option value="1">Klant</option>
+							<option value="1" selected>Klant</option>
 							<option value="2">Chauffeur</option>
 						</select>
 					</div>
 					<div class="col">
-						<h4>Openstaande aanvragen</h4>
+						<h3>Uw rithistorie</h3>
+						<ul>
+							<li>25/03/2018 Eindhoven</li>
+						</ul>
 					</div>
 				</div>
 			</div>
 			<div class="col-40px"></div>
 			<div class="col">
-	<div id="googleMap" style="width:100%;height:400px;"></div>
-
+				
 			</div>
 			<div class="col-40px"></div>
 		</div>
 	</div>
 
-<script>
-function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-</script>
+	<div id="footer" class="container bg-white">
+		<div class="row">
+			<div class="col-2"></div>
+			<div class="col-8">
+				<br/>
+				<h1>Aanpassen van uw gegevens</h1>
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDhUaFv5qwATzUG_DlgxbNCH1wXBa-B-PQ&callback=myMap"></script>
-
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+					natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+					eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, f ringilla vel, aliquet nec, vulputate
+					eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+					Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
+					porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+				</p>
+			</div>
+		</div>
+	</div>
 
 <?php
 include_once "assets/footer.php";
