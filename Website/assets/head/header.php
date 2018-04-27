87b5cd7ec30d97477819089718fdcc14c8e8e5e1
Sander Jochems <sanderjochems@hotmail.nl>
2018-04-27 15:09:44 +0200

    Bootstrap for Offline Use

--- Website/assets/head/header.php
+++ Website/assets/head/header.php
@@ -6,9 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php if (isset($pageTitle)) { echo $pageTitle; } else { echo "Pagina"; } ?> | Ixat Taxi's</title>
 
-    <!-- Bootstrap CSS & JS -->
+    <!-- Bootstrap CSS & JS
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" type="text/css" rel="stylesheet">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script> -->
+    <link href="/assets/bootstrap-4.1.0/dist/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <script src="/assets/bootstrap-4.1.0/dist/js/bootstrap.min.js"></script>
     <!-- Ixat CSS -->
     <link href="/assets/css/ixat.css" type="text/css" rel="stylesheet">
     <link href="/assets/css/ixat.1.css" type="text/css" rel="stylesheet">
