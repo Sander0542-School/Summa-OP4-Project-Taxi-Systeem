e44d02b09f0fbadc44286dc7927feb4a119f7449
Sander Jochems <sanderjochems@hotmail.nl>
2018-04-30 18:00:59 +0200

    Add UC_DATA var for when the user is a Chauffeur

--- Website/assets/head.php
+++ Website/assets/head.php
@@ -6,6 +6,11 @@ if ($CORE->is_logged_in())  {
   $stmt = $CORE->runQuery("SELECT * FROM klant WHERE gebruikersnaam=:gebruikersnaam");
   $stmt->execute(array(":gebruikersnaam"=>$_SESSION['userSession']));
   $U_DATA = $stmt->fetch(PDO::FETCH_ASSOC);
+  if ($U_DATA["chauffeurID"] != null) {
+    $stmt = $CORE->runQuery("SELECT * FROM chauffeur WHERE id=:cID");
+    $stmt->execute(array(":cID"=>$U_DATA["chauffeurID"]));
+    $UC_DATA = $stmt->fetch(PDO::FETCH_ASSOC);
+  }
 }
 
 include "head/header.php";
