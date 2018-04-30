4783253cd705159097d4ae2c2ca41d16be8e00c6
Sander Jochems <sanderjochems@hotmail.nl>
2018-04-30 13:51:56 +0200

    Create Login Function

--- WPF/IXAT/Database.cs
+++ WPF/IXAT/Database.cs
@@ -12,6 +12,33 @@ namespace IXAT
     {
         private MySqlConnection _conn = new MySqlConnection("Server=localhost;Database=ixat;Uid=root;Pwd=");
 
+        public bool Login(string sUsername, string sPassword)
+        {
+            bool bResult = false;
 
+            try
+            {
+                _conn.Open();
+
+                MySqlCommand sqlCommand = _conn.CreateCommand();
+                sqlCommand.CommandText = "SELECT * FROM logins WHERE username = @username AND password = @password";
+                sqlCommand.Parameters.AddWithValue("@username", sUsername);
+                sqlCommand.Parameters.AddWithValue("@password", sPassword);
+
+                MySqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+                bResult = dataReader.HasRows;
+            }
+            catch (Exception)
+            {
+                //Do nothing
+            }
+            finally
+            {
+                _conn.Close();
+            }
+
+            return bResult;
+        }
     }
 }
